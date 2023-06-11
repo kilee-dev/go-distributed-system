5648c5bfd3ebe2c6f6e3d6558a1de848accb7eec
ki <ki@Sazze.local>
2023-06-11 15:54:32 -0700

    Implement global unique ID generator

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module maelstrom-echo
 go 1.20
 
 require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230516124010-52951329816e
+
+require github.com/google/uuid v1.3.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
@@ -23,6 +24,35 @@ func main() {
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
 	})
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+		body["id"] = uuid.New().String()
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	// n.Handle("generate", func(msg maelstrom.Message) error {
+	// 	// Unmarshal the message body as an loosely-typed map.
+	// 	var body map[string]any
+	// 	if err := json.Unmarshal(msg.Body, &body); err != nil {
+	// 		return err
+	// 	}
+
+	// 	// Update the message type to return back.
+	// 	body["type"] = "generate_ok"
+
+	// 	// Echo the original message back with the updated message type.
+	// 	return n.Reply(msg, body)
+	// })
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
